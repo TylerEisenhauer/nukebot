eb14ecd2405a7dec5efb3326a4391560f7736262
Tyler Eisenhauer <tylereisenhauer7372@gmail.com>
2020-09-15 11:41:53 -0500

    uncomment code

--- src/index.ts
+++ src/index.ts
@@ -13,40 +13,40 @@ const prefix = '!'
 let cache: string[] = []
 let existingProductList: string[][] = []
 
-// const statusCheck = async () => {
-//     const browser = await puppeteer.launch()
-//     try {
-//         const page = await browser.newPage()
-//         await page.setViewport({
-//             width: 2560,
-//             height: 1440
-//         })
-//         await page.goto('https://novelkeys.xyz/collections/extras-group-buy')
-//         const newProductList: string[][] = await page.evaluate(() => {
-//             const products: string[][] = []
-//             document
-//                 .querySelectorAll('#Collection > ul > li')
-//                 .forEach((element: Element) => {
-//                     const product: string[] = [element.querySelector('div > div.one-whole > div').textContent, element.querySelector('#overlay_text')?.textContent]
-//                     products.push(product)
-//                 })
-//             return products.sort()
-//         })
-//         if (!isEqual(existingProductList, newProductList)) {
-//             existingProductList = newProductList
-//             await webhookClient.send(`<@${process.env.ME_ID}> Page change detected <https://novelkeys.xyz/collections/extras-group-buy>`, {
-//                 files: [await page.screenshot()]
-//             })
-//         }
-//     } catch (e) {
-//         console.log(e)
-//         await webhookClient.send(`Encountered an error\n${e}`)
-//     } finally {
-//         await browser.close()
-//     }
-//     setTimeout(statusCheck, 1000 * 60 * 10)
-// }
-// statusCheck()
+const statusCheck = async () => {
+    const browser = await puppeteer.launch()
+    try {
+        const page = await browser.newPage()
+        await page.setViewport({
+            width: 2560,
+            height: 1440
+        })
+        await page.goto('https://novelkeys.xyz/collections/extras-group-buy')
+        const newProductList: string[][] = await page.evaluate(() => {
+            const products: string[][] = []
+            document
+                .querySelectorAll('#Collection > ul > li')
+                .forEach((element: Element) => {
+                    const product: string[] = [element.querySelector('div > div.one-whole > div').textContent, element.querySelector('#overlay_text')?.textContent]
+                    products.push(product)
+                })
+            return products.sort()
+        })
+        if (!isEqual(existingProductList, newProductList)) {
+            existingProductList = newProductList
+            await webhookClient.send(`<@${process.env.ME_ID}> Page change detected <https://novelkeys.xyz/collections/extras-group-buy>`, {
+                files: [await page.screenshot()]
+            })
+        }
+    } catch (e) {
+        console.log(e)
+        await webhookClient.send(`Encountered an error\n${e}`)
+    } finally {
+        await browser.close()
+    }
+    setTimeout(statusCheck, 1000 * 60 * 10)
+}
+statusCheck()
 
 client.on('ready', async () => {
     await initializeAPIClients()
