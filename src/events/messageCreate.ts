753bfbdeeb358cd3245c20e4bfe620841cf274cd
Tyler Eisenhauer <tylereisenhauer7372@gmail.com>
2023-04-22 18:25:05 -0500

    formatting

--- src/events/messageCreate.ts
+++ src/events/messageCreate.ts
@@ -7,31 +7,31 @@ import Sentiment from 'sentiment'
 const prefix = '!'
 
 export async function messageCreate(message: Message) {
-    if (message.author.bot) return
-    const client: ExtendedClient = message.client
-    const performGamaAlert: boolean = process.env.PERFORM_GAMA_ALERT === 'true'
-    const sentiment = new Sentiment()
+  if (message.author.bot) return
+  const client: ExtendedClient = message.client
+  const performGamaAlert: boolean = process.env.PERFORM_GAMA_ALERT === 'true'
+  const sentiment = new Sentiment()
 
-    if (performGamaAlert && message.author.id === process.env.GAMA_ID && message.channel.id === process.env.ALERT_CHANNEL_ID) {
-        await client.commands.get('gamapost').execute([], message)
-    }
-    if (message.author.id === process.env.PWN_ID && message.channel.id === process.env.ALERT_CHANNEL_ID) {
-        await client.commands.get('pwnpost').execute([], message)
-    }
-    if (message.channelId === process.env.HEALER_CHANNEL_ID) {
-      const result = sentiment.analyze(message.content,{
-        extras: {
-          'pog': 3
-        }
-      })
-      if (result.comparative < .3) await message.react('❤️')
-    }
+  if (performGamaAlert && message.author.id === process.env.GAMA_ID && message.channel.id === process.env.ALERT_CHANNEL_ID) {
+    await client.commands.get('gamapost').execute([], message)
+  }
+  if (message.author.id === process.env.PWN_ID && message.channel.id === process.env.ALERT_CHANNEL_ID) {
+    await client.commands.get('pwnpost').execute([], message)
+  }
+  if (message.channelId === process.env.HEALER_CHANNEL_ID) {
+    const result = sentiment.analyze(message.content, {
+      extras: {
+        'pog': 3
+      }
+    })
+    if (result.comparative < .3) await message.react('❤️')
+  }
 
-    if (!message.content.startsWith(prefix)) return
+  if (!message.content.startsWith(prefix)) return
 
-    const args: string[] = parseArgs(prefix, message.content)
-    const command: string = args.shift().toLowerCase()
+  const args: string[] = parseArgs(prefix, message.content)
+  const command: string = args.shift().toLowerCase()
 
-    if (command.startsWith(prefix)) return
-    await client.commands.get(command)?.execute(args, message)
+  if (command.startsWith(prefix)) return
+  await client.commands.get(command)?.execute(args, message)
 }
